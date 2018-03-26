7ab76511bd9fce8896c3c642cbe429c6b3614604
pradeeppadmarajaiahclear <pradeep.padmarajaiah@gmail.com>
2018-03-26 14:39:54 +0530

    ''

--- src/main/java/com/dashboard/DashboardController.java
+++ src/main/java/com/dashboard/DashboardController.java
@@ -11,7 +11,7 @@ import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.Controller;
+import org.springframework.stereotype.Controller;
 
 @Controller
 public class DashboardController {
